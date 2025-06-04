075eeb225c3174d42600eed93a706831f11c1de4
marianaaa05 <nanaarredondo220@gmail.com>
2025-06-04 13:34:19 -0500

    refactor: update Prisma client initialization and comment out output path in schema

--- lib/db.ts
+++ lib/db.ts
@@ -5,8 +5,11 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+// export const db = globalThis.prisma || new PrismaClient();
+const prisma = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
-  globalThis.prisma = db;
+  globalThis.prisma = prisma;
 }
+
+export const db = prisma;
