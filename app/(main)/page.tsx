d9715597d1ea2f428c24204f4d846f22168e4180
marianaaa05 <nanaarredondo220@gmail.com>
2025-06-08 19:06:59 -0500

    feat: update Header and Home components with new branding and event section

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
  export default function Home() {
    return (
      <>
-       <div className="w-full h-full text-center p-6 md:p-8 lg:p-12">
+       {/* <div className="w-full h-full text-center p-6 md:p-8 lg:p-12">
          <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-neutral-100 mb-6">
            INNOVACIÓN EN TECNOLOGÍA
          </h1>
@@ -15,40 +15,59 @@ import Image from "next/image";
            <Database className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12" />
            <Server className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12" />
          </div>
-       </div>
+       </div> */}
  
        <div className="max-w-screen-lg mx-auto flex flex-col lg:flex-row items-center justify-center p-6 gap-6 lg:gap-12">
          <div className="relative w-60 h-60 md:w-80 md:h-80 lg:w-96 lg:h-96 mb-6 lg:mb-0 flex flex-col items-center">
-           <h2 className="text-lg md:text-xl lg:text-2xl font-bold text-neutral-100 text-center mb-4">
+           {/* <h2 className="text-lg md:text-xl lg:text-2xl font-bold text-neutral-100 text-center mb-4">
              Semilleros de Investigación
-           </h2>
+           </h2> */}
+           <h2 className="text-4xl md:text-2xl lg:text-3xl font-extrabold text-center mb-4 text-fuchsia-100 drop-shadow-[0_0_10px_#ff00ff] drop-shadow-[2px_2px_10px_#00ffff]">
+            SEMILLEROS DE INVESTIGACIÓN
+          </h2>
+
            <div className="flex flex-row justify-center gap-6 lg:gap-8">
              <Image
-               src="/next.svg"
-               width={100}
-               height={100}
-               alt="Logo 1"
-               className="object-contain"
+               src="/Kernel.png"
+               width={140}
+               height={140}
+               alt="Semillero de Investigación en Intelligence Artificial y Desarrollo de Software"
+               className="object-contain drop-shadow-2xl"
              />
              <Image
-               src="/next.svg"
-               width={100}
-               height={100}
-               alt="Logo 2"
-               className="object-contain"
+               src="/sinapsis.png"
+               width={90}
+               height={90}
+               alt="Semillero de Investigación en Robótica"
+               className="object-contain drop-shadow-2xl"
              />
              <Image
-               src="/next.svg"
-               width={100}
-               height={100}
-               alt="Logo 3"
-               className="object-contain"
+               src="/animus.png"
+               width={80}
+               height={80}
+               alt="Semillero de Cyber Seguridad  Animus Cracking"
+               className="object-contain drop-shadow-2xl"
              />
            </div>
          </div>
        </div>
        {/* <VideoComponent /> */}
        <div className="mb-8 mt-8">
+
+        <div className="min-h-screen w-full text-center p-6 md:p-8 lg:p-12">
+          <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-neutral-100 mb-6">
+            EVENTOS
+          </h1>
+          <h2 className="text-neutral-50">
+            (aqui adaptar un carrusel de eventos responsive para todas las
+            pantallas)
+          </h2>
+          <div className="flex justify-center gap-6 md:gap-8 text-white">
+            <Code className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12" />
+            <Database className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12" />
+            <Server className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12" />
+          </div>
+        </div>
  
        </div>
        {/* <div className="flex flex-col items-center p-8 gap-y-2 w-full">
