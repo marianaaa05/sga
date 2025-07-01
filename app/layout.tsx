714907eaa3e37a5de29934eb6ba779d78ec7a335
marianaaa05 <nanaarredondo220@gmail.com>
2025-07-01 14:45:48 -0500

    Install Fuentes Fix error reload vercel geistSans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,53 @@
 import type { Metadata } from "next";
- import { Geist, Geist_Mono } from "next/font/google";
- import "./globals.css";
- import { ClerkProvider } from "@clerk/nextjs";
- import { ToasterProvider } from "@/components/providers/toaster-provider";
- 
- const geistSans = Geist({
-   variable: "--font-geist-sans",
-   subsets: ["latin"],
- });
- 
- const geistMono = Geist_Mono({
-   variable: "--font-geist-mono",
-   subsets: ["latin"],
- });
- 
- export const metadata: Metadata = {
-   title: "Plataforma LMS - Cursos AUNAR Pasto",
-   description: "Creada por estudiantes de Semilleros de Investigación de Aunar Pasto",
- };
- 
- export default function RootLayout({
-   children,
- }: Readonly<{
-   children: React.ReactNode;
- }>) {
-   return (
-     <ClerkProvider>
-     <html lang="en">
-       <body
-         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-       >
-        <ToasterProvider />
-         {children}
-       </body>
-     </html>
-     </ClerkProvider>
-   );
- }
+// import { Geist, Geist_Mono } from "next/font/google";
+import localFonts from "next/font/local";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ToasterProvider } from "@/components/providers/toaster-provider";
+
+const geistSans = localFonts({
+  src: [
+    {
+      path: '../public/fonts/Geist-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Geist-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: "--font-geist-sans",
+  display: "swap",
+});
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+export const metadata: Metadata = {
+  title: "Plataforma LMS - Cursos AUNAR Pasto",
+  description:
+    "Creada por estudiantes de Semilleros de Investigación de Aunar Pasto",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable}antialiased`}
+        >
+          <ToasterProvider />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
