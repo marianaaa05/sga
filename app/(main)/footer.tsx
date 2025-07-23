43ba0ead57696ee47d1356f5f91ed43a6ea5fcd0
marianaaa05 <nanaarredondo220@gmail.com>
2025-07-23 13:20:03 -0500

    feat: mejorar el footer y navbar, actualizando estilos y estructura para una mejor presentación

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -5,42 +5,49 @@ import Link from "next/link";
 export const Footer = () => {
   return (
     // <footer className="hidden lg:block w-full bg-gradient-to-r from-gray-400 via-gray-700 to-gray-400">
-    <footer className="w-full lg:w-full mx-auto items-center justify-between h-full bg-gradient-to-r from-gray-400 via-gray-700 to-gray-400">
-      {/* Contenedor principal */}
-      <div
-        className="max-w-screen mx-auto flex items-center justify-start sm:justify-center p-2"
-      >
-        <Button size="lg" variant="link">
+    <footer className="w-full border-t bg-gradient-to-br from-slate-100 to-slate-300 dark:from-slate-800 dark:to-slate-700 shadow-sm">
+      <div className="max-w-screen-xl mx-auto flex flex-wrap items-center justify-center lg:justify-end gap-4 lg:gap-28 px-4 lg:px-20 lg:pl-32 py-4">
+        <Button size="lg" variant="link" className="p-0">
+          <Image
+            src="/skahverse.png"
+            alt="Semillero de Inteligencia Artificial y Desarrollo de Software"
+            height={70}
+            width={70}
+            className="object-contain"
+          />
+        </Button>
+
+        <Button size="lg" variant="link" className="p-0">
           <Image
             src="/Kernel.png"
             alt="Semillero de Inteligencia Artificial y Desarrollo de Software"
-            height={100}
-            width={100}
-            className="mr-4 mt-3"
-          ></Image>
+            height={70}
+            width={70}
+            className="object-contain"
+          />
         </Button>
 
-        <Button size="lg" variant="link">
+        <Button size="lg" variant="link" className="p-0">
           <Image
             src="/Animus.png"
             alt="Semillero de ciberseguridad"
             height={50}
             width={50}
-            className="mr-3"
-          ></Image>
+            className="object-contain"
+          />
         </Button>
 
-        <Button size="lg" variant="link">
+        <Button size="lg" variant="link" className="p-0">
           <Image
             src="/sinapsis.png"
             alt="Semillero de Investigación en Robótica"
             height={50}
             width={50}
-            className="mr-4 inset-shadow-black"
-          ></Image>
+            className="object-contain"
+          />
         </Button>
 
-        <Button size="lg" variant="link" className="hidden sm:block">
+        <Button size="lg" variant="link" className="hidden sm:flex p-0">
           <Link
             href="https://www.aunar.edu.co/"
             target="_blank"
@@ -51,22 +58,23 @@ export const Footer = () => {
               alt="Corporación Universitaria Autónoma de Nariño"
               height={45}
               width={45}
-              className="mr-3"
-            ></Image>
+              className="object-contain"
+            />
           </Link>
         </Button>
 
-        <Button size="lg" variant="link" className="hidden sm:block">
+        <Button size="lg" variant="link" className="hidden sm:flex p-0">
           <Image
             src="/logo_aunar.svg"
-            alt=""
-            height={200}
-            width={200}
-            className="mr-3"
-          ></Image>
+            alt="Logo AUNAR"
+            height={130}
+            width={120}
+            className="object-contain"
+          />
         </Button>
       </div>
-      <p className="w-full text-xs text-neutral-100 text-center p-2 bg-gradient-to-r from-gray-500 via-gray-900 to-gray-500">
+
+      <p className="w-full text-xs text-center text-slate-700 p-2 lg:w-full mx-auto items-center justify-between h-full border bg-gradient-to-br from-slate-100 to-slate-300 dark:from-slate-800 dark:to-slate-700 shadow-sm">
         © 2025 Semilleros de Investigación en Ingeniería Informática. Todos los
         derechos reservados.
       </p>
--- app/dashboard/_components/navbar.tsx
+++ app/dashboard/_components/navbar.tsx
@@ -3,8 +3,17 @@ import { MobileSidebar } from "./mobile-sidebar";
 
 const Navbar = () => {
   return (
-    <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
-      <MobileSidebar />
+    <div className="p-4 border-b h-full flex items-center justify-between border bg-gradient-to-br from-slate-100 to-slate-300 dark:from-slate-800 dark:to-slate-700 shadow-sm">
+      <div className="flex items-center gap-4">
+        <MobileSidebar />
+        <h1 className="text-2xl sm:text-xl md:text-2xl lg:text-2xl font-extrabold tracking-wide">
+          <span className="bg-gradient-to-r from-[#00ccff] via-[#ff00ff] to-[#ff6600] bg-clip-text text-transparent">
+            SKAH
+          </span>
+          <span className="text-sky-800">verse</span>
+        </h1>
+      </div>
+
       <NavbarRoutes />
     </div>
   );
