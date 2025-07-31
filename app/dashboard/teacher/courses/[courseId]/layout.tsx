fb55b06556421eff39bf884f6b89128afcdb597f
marianaaa05 <nanaarredondo220@gmail.com>
2025-07-30 20:48:32 -0500

    feat: actualizar CourseLayout para manejar parámetros de forma asíncrona y regla de Eslint - Ignorar a Prisma archivos generados

--- app/dashboard/teacher/courses/[courseId]/layout.tsx
+++ app/dashboard/teacher/courses/[courseId]/layout.tsx
@@ -1,13 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { ReactNode } from "react";
 
 interface LayoutProps {
   children: ReactNode;
-  params: {
-    courseId: string;
-  };
+  params: Promise<{ courseId: string }>;
 }
 
-export default function CourseLayout({ children }: LayoutProps) {
+export default async function CourseLayout({ children, params }: LayoutProps) {
+  const { courseId } = await params;
   return (
     <div>
       {children}
